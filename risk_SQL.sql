177550cc63a4ef18a629171f5470e39540cc8df1
G3lj4s <angelcg225@gmail.com>
2024-10-24 20:37:04 +0200

    actualizacion base de datos

--- risk_SQL.sql
+++ risk_SQL.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 14-10-2024 a las 21:40:42
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 24-10-2024 a las 18:35:39
+-- Versión del servidor: 8.3.0
+-- Versión de PHP: 8.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,45 +24,132 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuario`
+-- Estructura de tabla para la tabla `partidas`
 --
 
-CREATE TABLE `usuario` (
-  `id` int(11) NOT NULL,
-  `name` varchar(200) NOT NULL,
-  `email` varchar(200) NOT NULL,
-  `password` varchar(30) NOT NULL,
-  `admin` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `partidas`;
+CREATE TABLE IF NOT EXISTS `partidas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_usuario` int NOT NULL,
+  `estado` tinyint(1) NOT NULL COMMENT '0 - creado\r\n1-distribuido\r\n2-ganada\r\n3-perdida',
+  `num_tropas` int NOT NULL,
+  `ultimo_jugador` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_partidas_usuarios_id` (`id_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Volcado de datos para la tabla `usuario`
+-- Volcado de datos para la tabla `partidas`
 --
 
-INSERT INTO `usuario` (`id`, `name`, `email`, `password`, `admin`) VALUES
+INSERT INTO `partidas` (`id`, `id_usuario`, `estado`, `num_tropas`, `ultimo_jugador`) VALUES
+(52, 1, 1, 30, -1),
+(54, 1, 1, 30, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `territorios`
+--
+
+DROP TABLE IF EXISTS `territorios`;
+CREATE TABLE IF NOT EXISTS `territorios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `posicion` int NOT NULL,
+  `nombre` varchar(200) NOT NULL,
+  `propietario` int NOT NULL,
+  `id_partida` int NOT NULL,
+  `tropas` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_territorios_partidas_id` (`id_partida`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=593 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `territorios`
+--
+
+INSERT INTO `territorios` (`id`, `posicion`, `nombre`, `propietario`, `id_partida`, `tropas`) VALUES
+(533, 1, 'Suecia', -1, 52, 2),
+(534, 2, 'Vietnam', 1, 52, 2),
+(535, 3, 'Irán', 1, 52, 1),
+(536, 4, 'Nigeria', 1, 52, 2),
+(537, 5, 'Egipto', -1, 52, 1),
+(538, 6, 'Colombia', 1, 52, 1),
+(539, 7, 'Brasil', 1, 52, 2),
+(540, 8, 'Venezuela', 1, 52, 1),
+(541, 9, 'Alemania', -1, 52, 4),
+(542, 10, 'Japón', -1, 52, 1),
+(543, 11, 'Chile', 1, 52, 1),
+(544, 12, 'India', -1, 52, 2),
+(545, 13, 'Vietnam', 1, 52, 1),
+(546, 14, 'Indonesia', 1, 52, 1),
+(547, 15, 'Rusia', 1, 52, 1),
+(548, 16, 'Mongolia', -1, 52, 1),
+(549, 17, 'Finlandia', -1, 52, 1),
+(550, 18, 'Chile', -1, 52, 1),
+(551, 19, 'Islandia', -1, 52, 1),
+(552, 20, 'Finlandia', -1, 52, 1),
+(573, 1, 'Australia', 1, 54, 10),
+(574, 2, 'Perú', -1, 54, 1),
+(575, 3, 'Irán', -1, 54, 2),
+(576, 4, 'Noruega', -1, 54, 1),
+(577, 5, 'Alaska', 1, 54, 12),
+(578, 6, 'Noruega', 1, 54, 8),
+(579, 7, 'China', 1, 54, 9),
+(580, 8, 'Mongolia', 1, 54, 11),
+(581, 9, 'Siberia', 1, 54, 11),
+(582, 10, 'Siria', -1, 54, 1),
+(583, 11, 'Vietnam', 1, 54, 6),
+(584, 12, 'Francia', 1, 54, 8),
+(585, 13, 'Rusia', -1, 54, 1),
+(586, 14, 'Grecia', 1, 54, 11),
+(587, 15, 'Groenlandia', -1, 54, 2),
+(588, 16, 'Reino Unido', -1, 54, 1),
+(589, 17, 'Sudáfrica', 1, 54, 15),
+(590, 18, 'Grecia', -1, 54, 1),
+(591, 19, 'Grecia', -1, 54, 3),
+(592, 20, 'Canadá', 1, 54, 10);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
+  `admin` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `name`, `email`, `password`, `admin`) VALUES
 (1, 'pepe', 'pepe@gmail.com', '1234', 1),
-(2, 'angelito', 'angelcg225@gmail.com', '6447', 1);
+(6, 'angel', 'angelcg225@gmail.com', '4321', 1);
 
 --
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
+-- Restricciones para tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `usuario`
+-- Filtros para la tabla `partidas`
 --
-ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `partidas`
+  ADD CONSTRAINT `fk_partidas_usuarios` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Filtros para la tabla `territorios`
+--
+ALTER TABLE `territorios`
+  ADD CONSTRAINT `fk_tableros_partidas_id` FOREIGN KEY (`id_partida`) REFERENCES `partidas` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
