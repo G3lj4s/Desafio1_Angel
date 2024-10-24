56a8d10232a91e6a6cd72b285277377582c260e6
G3lj4s <angelcg225@gmail.com>
2024-10-24 20:29:50 +0200

    refactorizacion clase controladoUsuario

--- Controlador/ControladorUsuario.php
+++ Controlador/ControladorUsuario.php
@@ -4,53 +4,9 @@
 class ControladorUsuario{
     public static function mostrarPerfil($datosRecibidos){
         $datosRecibidos = json_decode($datosRecibidos, true);
-        
-        if (isset($datosRecibidos['email']) && isset($datosRecibidos['password'])) {
-            $usuario = ConexionBDUsuario::obtenerUsuarioEmail($datosRecibidos['email']);
-            
-            if ($usuario != null) {
-                if ($usuario->getPassword() == $datosRecibidos['password']) {
-                    echo json_encode($usuario);
-                } else {
-                    echo json_encode(['message' => 'la password del usuario no es correcta']);
-                }
-            } else {
-                echo json_encode(['message' => 'este usuario no existe en el sistema']);
-            }
-        } else {
-            echo json_encode(['message' => 'datos incompletos']);
-        }
-    }
-    public static function cambiarPassword($datosRecibidos){
-        $datosRecibidos = json_decode($datosRecibidos, true);
-        
-        if (isset($datosRecibidos['email']) && isset($datosRecibidos['password'])) {
-            $usuario = ConexionBDUsuario::obtenerUsuarioEmail($datosRecibidos['email']);
-            
-            if ($usuario != null) {
-                if ($usuario->getPassword() == $datosRecibidos['password']) {
-                    $usuario->generatePassword();
-                    ConexionBDUsuario::actualizarUsuario($usuario);
-                    $resultado = ConexionMail::mandarNuevaPassword($usuario);
-                    if ($resultado) {
-                        echo json_encode(['message' => 'revise su correo para ver su nueva password']);
-                    }else{
-                        echo json_encode(['message' => $resultado]);
-                    }
-                } else {
-                    echo json_encode(['message' => 'la password del usuario no es correcta']);
-                }
-            } else {
-                echo json_encode(['message' => 'este usuario no existe en el sistema']);
-            }
-        } else {
-            echo json_encode(['message' => 'datos incompletos']);
-        }
-    }
-    public static function mostrarStats($datosRecibidos){
-        $datosRecibidos = json_decode($datosRecibidos, true);
     
         if (!isset($datosRecibidos['email']) || !isset($datosRecibidos['password'])) {
+            http_response_code(400);
             echo json_encode(['message' => 'datos incompletos']);
             return;
         }
@@ -58,21 +14,83 @@ class ControladorUsuario{
         $usuario = ConexionBDUsuario::obtenerUsuarioEmail($datosRecibidos['email']);
     
         if ($usuario == null) {
+            http_response_code(404);
             echo json_encode(['message' => 'este usuario no existe en el sistema']);
             return;
         }
     
         if ($usuario->getPassword() != $datosRecibidos['password']) {
+            http_response_code(401);
             echo json_encode(['message' => 'la password del usuario no es correcta']);
             return;
         }
+        http_response_code(200);
+        echo json_encode($usuario);
+    }
+    
+    public static function cambiarPassword($datosRecibidos){
+        $datosRecibidos = json_decode($datosRecibidos, true);
+    
+        if (!isset($datosRecibidos['email']) || !isset($datosRecibidos['password'])) {
+            http_response_code(400);
+            echo json_encode(['message' => 'datos incompletos']);
+            return;
+        }
+    
+        $usuario = ConexionBDUsuario::obtenerUsuarioEmail($datosRecibidos['email']);
+    
+        if ($usuario == null) {
+            http_response_code(404);
+            echo json_encode(['message' => 'este usuario no existe en el sistema']);
+            return;
+        }
+    
+        if ($usuario->getPassword() != $datosRecibidos['password']) {
+            http_response_code(401);
+            echo json_encode(['message' => 'la password del usuario no es correcta']);
+            return;
+        }
+        
+        $usuario->generatePassword();
+        ConexionBDUsuario::actualizarUsuario($usuario);
+        $resultado = ConexionMail::mandarNuevaPassword($usuario);
+        http_response_code(200);
+        echo json_encode(['message' => 'la contraseña ha sido cambiada y enviada por correo']);
+    }
+    
+    public static function mostrarStats($datosRecibidos){
+        $datosRecibidos = json_decode($datosRecibidos, true);
+    
+        if (!isset($datosRecibidos['email']) || !isset($datosRecibidos['password'])) {
+            http_response_code(400);
+            echo json_encode(['message' => 'datos incompletos']);
+            return;
+        }
+    
+        $usuario = ConexionBDUsuario::obtenerUsuarioEmail($datosRecibidos['email']);
+    
+        if ($usuario == null) {
+            http_response_code(404);
+            echo json_encode(['message' => 'este usuario no existe en el sistema']);
+            return;
+        }
+    
+        if ($usuario->getPassword() != $datosRecibidos['password']) {
+            http_response_code(401);
+            echo json_encode(['message' => 'la password del usuario no es correcta']);
+            return;
+        }
+    
         $partidasEmpezadas = ConexionBDPartida::numPartidasPorEstado($usuario->getId(), 1); 
         $partidasGanadas = ConexionBDPartida::numPartidasPorEstado($usuario->getId(), 2);
         $partidasPedidas = ConexionBDPartida::numPartidasPorEstado($usuario->getId(), 3);
+        
+        http_response_code(200);
         echo json_encode([
             'Empezadas' => $partidasEmpezadas,
-            'Ganadas'=> $partidasGanadas,
-            'Perdidas'=> $partidasPedidas
+            'Ganadas' => $partidasGanadas,
+            'Perdidas' => $partidasPedidas
         ]);
     }
+    
 }
